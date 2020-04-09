3dacedbcdad4ff0662be1b748e10f3b782dd21e7
Max Hawkins <1227297+thinkybeast@users.noreply.github.com>
2020-04-09 11:05:52 -0700

    create AddVideoButton component

--- src/components/dashboard/AddVideo.jsx
+++ src/components/dashboard/AddVideo.jsx
@@ -1,34 +1,10 @@
 import React from "react";
+import AddVideoButton from "./AddVideoButton";
 
 const AddVideo = (props) => {
   return (
     <div>
-      <div id="add-video" className="d-flex">
-        <svg
-          className="bi bi-plus-square"
-          viewBox="0 0 16 16"
-          xmlns="http://www.w3.org/2000/svg"
-          data-toggle="modal"
-          data-target="#upload-video-modal"
-        >
-          <path
-            fillRule="evenodd"
-            d="M8 3.5a.5.5 0 01.5.5v4a.5.5 0 01-.5.5H4a.5.5 0 010-1h3.5V4a.5.5 0 01.5-.5z"
-            clipRule="evenodd"
-          />
-          <path
-            fillRule="evenodd"
-            d="M7.5 8a.5.5 0 01.5-.5h4a.5.5 0 010 1H8.5V12a.5.5 0 01-1 0V8z"
-            clipRule="evenodd"
-          />
-          <path
-            fillRule="evenodd"
-            d="M14 1H2a1 1 0 00-1 1v12a1 1 0 001 1h12a1 1 0 001-1V2a1 1 0 00-1-1zM2 0a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V2a2 2 0 00-2-2H2z"
-            clipRule="evenodd"
-          />
-        </svg>
-        <p>Add a video</p>
-      </div>
+      <AddVideoButton />
       <div
         className="modal fade"
         id="upload-video-modal"
