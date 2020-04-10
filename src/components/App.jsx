3c4b61946b9ef3d7ce74d4eb1217885dc2ce53af
Max Hawkins <1227297+thinkybeast@users.noreply.github.com>
2020-04-10 10:52:22 -0700

    send 404s and logo clicks to home path

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Switch, Route, BrowserRouter, Redirect } from "react-router-dom";
 import Header from "./Header";
 import Dashboard from "./dashboard/Dashboard";
 import Video from "./video/Video";
@@ -16,6 +16,9 @@ class App extends Component {
           </Route>
           <Route path="/video">
             <Video />
+          </Route>
+          <Route path="*">
+            <Redirect to="/" />
           </Route>
         </Switch>
       </BrowserRouter>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../images/bento_logo.png";
 
 const Header = () => {
   return (
     <header className="container">
       <div className="logo">
-        <img src={logo} />
+        <Link to="/">
+          <img src={logo} />
+        </Link>
       </div>
     </header>
   );
